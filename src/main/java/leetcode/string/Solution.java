072efab853707d46bb619357d308554b05c0e697
zxm66 <1396729865@qq.com>
2020-08-07 16:46:45 +0800

    baiud,con

---
+++ src/main/java/leetcode/string/Solution.java
@@ -0,0 +1,26 @@
+
+public class Solution{
+	
+    public void reverseString(char[] s) {
+		int i = 0;
+		int j = s.length - 1;
+		char c ;
+		while(i < j){
+			c = s[i] ;
+			s[i] = s[j];
+			s[j] = c;
+			i ++;
+			j --;
+		}
+		
+    }
+	public static void main(String[] args) {
+		char[] helloWorld = {'h','w'};
+		System.out.println(helloWorld);
+		new Solution().reverseString(helloWorld);
+		System.out.println(helloWorld);
+		
+	}
+
+
+}
