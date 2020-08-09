cd2c896f47e6710e07f569fe213d5ca111af7d72
zxm66 <1396729865@qq.com>
2020-08-09 18:22:48 +0800

    zxm

--- src/main/java/leetcode/string/LeetcodeLongestCommonPrefix.java
+++ src/main/java/leetcode/string/LeetcodeLongestCommonPrefix.java
@@ -1,5 +1,10 @@
 public class LeetcodeLongestCommonPrefix{
-	public static void main(String[] args)  {
+
+	public static void main(String[] args) {
 		System.out.println("hello world");
+		Map<Object, Object> map = new HashMap<Object, Object>();
+
+
 	}
+	
 }
