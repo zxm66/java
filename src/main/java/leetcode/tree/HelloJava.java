f36cc018b21185598b437ef22ac20731567b6b54
zxm66 <1396729865@qq.com>
2020-08-31 00:01:30 +0800

    gc

--- src/main/java/leetcode/tree/HelloJava.java
+++ src/main/java/leetcode/tree/HelloJava.java
@@ -2,6 +2,8 @@ package leetcode.tree;
 import java.util.HashMap;
 public class HelloJava{
 	public static void main(String[] args)  {
+		// current the coc-java langlage server does not works and 
+		
 		System.out.println("hello java");
 		HashMap<String,Object> map = new HashMap<String,Object>();
 		System.out.println(map);
